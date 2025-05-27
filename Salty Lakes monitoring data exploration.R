155010d60c61bd8543b84a148b62ece02ba3810a
Jackalyn-Wyrobek <129195527+Jackalyn-Wyrobek@users.noreply.github.com>
2025-05-27 16:00:53 -0500

    new tabs and graphs + some graph reorganization

--- Salty Lakes monitoring data exploration.R
+++ Salty Lakes monitoring data exploration.R
@@ -49,14 +49,14 @@ ui <- fluidPage(
                mainPanel(
                  plotOutput("lake_depth_boxplot")
                )
-             ), #close sidebar panel
+             ) #close sidebar panel
     ),
-    tabPanel("Exploring the role of road salt on nutrients",
+    tabPanel("Exploring the role of road salt by lake",
              sidebarLayout(
                sidebarPanel(
                  p("Select the variable you want to plot in relation to chloride concentration:"),
                  selectInput("lake_chloride",label="Lake",choices=lakes,multiple=T),
-                 selectInput("variable_chloride",label="Variable",choices=variables[1:9]), #limits choices to everything but Cl
+                 selectInput("variable_chloride",label="Variable",choices=variables[1:14]), #limits choices to everything but Cl
                  p("Select what depth you want to look at:"),
                  checkboxGroupInput(inputId = "depth_chloride",
                                     label= "Depth",
@@ -64,6 +64,17 @@ ui <- fluidPage(
                ), #close sidebar panel
                mainPanel(
                  plotOutput("chloride_plot")
+               )
+             ) #close sidebar layout
+    ), #close tab panel
+    tabPanel("Exploring the role of road salt by risk level",
+             sidebarLayout(
+               sidebarPanel(
+                 p("Select the variable you want to compare between risk levels:"),
+                 selectInput("y_variable_risk_level",label="Variable",choices=variables),
+               ),
+               mainPanel(
+                 plotOutput("risk_level_boxplot")
                )
              ), #close sidebar layout
              sidebarLayout(
@@ -82,14 +93,23 @@ ui <- fluidPage(
                                          `DOC (mg C/L)`,
                                          `DSi (mg SiO2/L)`),
                                 selected = "Chl-a (ug/L)"),
-                 ), #close sidebar panel
+               ), #close sidebar panel
                mainPanel(
                  plotOutput("chloride_plot2")
-                 )
-               ) #close sidebar layout
-             
-             
-    ) #close tab panel
+               )
+             ) #close sidebar layout
+    ), #close tab panel
+    tabPanel("Exploring the role of road salt by region",
+             sidebarLayout(
+               sidebarPanel(
+                 p("Select the variable you want to plot:"),
+                 selectInput("region_variable",label="Variable",choices=variables),
+               ),
+               mainPanel(
+                 plotOutput("region_boxplot")
+               )
+             ) #close sidebar panel
+    )
   ) #close all panels
 ) #close UI
 
@@ -130,7 +150,7 @@ server <- function(input, output, session) {
   chloride_data <- reactive({
     waterchem_v2 %>%
       pivot_wider(names_from=variable,values_from=value) %>%
-      pivot_longer(!c(lake,Date,Depth,`Cl- (ug/L)`),names_to="variable") %>%
+      pivot_longer(!c(lake,Risk_Level,Region,Date,Depth,`Cl- (ug/L)`),names_to="variable") %>%
       dplyr::filter(lake %in% input$lake_chloride,
                     variable %in% input$variable_chloride,
                     Depth %in% input$depth_chloride) 
@@ -143,13 +163,46 @@ server <- function(input, output, session) {
       theme_classic(base_size=14)
   })
   
+  risk_level_data <- reactive({
+    waterchem_v2 %>%
+      mutate(Risk_Level = fct_relevel(Risk_Level, 
+                                "Impacted", "At Risk", "Least Impacted")) %>%
+      dplyr::filter(variable %in% input$y_variable_risk_level)
+  })
+  
+  output$risk_level_boxplot <- renderPlot({
+    ggplot(risk_level_data(),aes(y=value,x=Risk_Level, color=Risk_Level))+
+      geom_boxplot()+
+      theme_classic(base_size=14) +
+      ylab("Value") + xlab("Risk Level")
+  })
+  
   output$chloride_plot2 <- renderPlot({
-    ggplot(data= waterchem,
+    ggplot(data= waterchem %>%
+             mutate(Risk_Level = fct_relevel(Risk_Level, 
+                                             "Impacted", "At Risk", "Least Impacted")),
            aes(x=`Cl- (ug/L)`, y=!!input$y_variable_chloride2, color=Depth))+
       geom_point()+
       facet_wrap(~Risk_Level, nrow=2) +
       theme_classic(base_size=14)
   })
+  
+  region_data <- reactive({
+    waterchem_v2 %>%
+      mutate(Region = fct_relevel(Region, 
+                                      "North Metro", "East Metro", "South Metro", "West Metro", "Central MN")) %>%
+      dplyr::filter(variable %in% input$region_variable)
+  })
+  
+  output$region_boxplot <- renderPlot({
+    ggplot(region_data(),aes(y=value,x=lake, color=lake))+
+      geom_boxplot()+
+      facet_wrap(~Region, scales="free_x", nrow=2) +
+      theme_classic(base_size=14) +
+      theme(axis.text.x=element_text(angle=40, hjust=1)) +
+      ylab("Value") + xlab("") 
+  })  
+  
   
 }
 
